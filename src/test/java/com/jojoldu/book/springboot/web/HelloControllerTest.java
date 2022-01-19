8584241a50d5e1aabad8276cd7a0a1f8b3d35eb6
ok0749 <63099945+ok0749@users.noreply.github.com>
2022-01-19 10:12:16 +0900

    lombok 설치 및 사용
    gradle version 7.1이라build.gradle 파일에
    implementation, annotationProcessor, testImplementation,
        testAnnotationProcessor 추가
    import static org.hamcrest.Matchers.is 부분
    자동완성 되지 않아 직접 입력

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -7,9 +7,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 // 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킨다.
 // 여기서는 SpringRunner라는 스프링 실행자를 사용
@@ -40,5 +41,18 @@ public class HelloControllerTest {
                 // 응답 본문 내용 검증
                 // Controller에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
