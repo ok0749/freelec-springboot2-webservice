5f160c96a8202532cb3b0eb07dd86afa9d562d15
ok0749 <63099945+ok0749@users.noreply.github.com>
2022-01-20 23:01:18 +0900

    전체 조회

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,8 +1,12 @@
 package com.jojoldu.book.springboot.domain.posts;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 // JpaRepository<Entity 클래스, PK 타입>를 상속시 기본적인 CRUD 메소드 자동 생성
 public interface PostsRepository extends JpaRepository<Posts, Long> {
-
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
