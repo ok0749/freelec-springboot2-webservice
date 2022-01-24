977683a91016ef02fc06fea5b16fbfdb16a90d5b
ok0749 <63099945+ok0749@users.noreply.github.com>
2022-01-24 20:02:34 +0900

    최신 코드로 변경

--- src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,18 +1,18 @@
 package com.jojoldu.book.springboot.domain.posts;
 
-import org.junit.After;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 // 별다른 설정 없이 사용할 경우 H2 데이터베이스를 자동으로 실행
 @SpringBootTest
 public class PostsRepositoryTest {
@@ -23,7 +23,7 @@ public class PostsRepositoryTest {
     // Junit에서 단위 테스트가 끝날 때마다 수행되는 메소드를 지정
     // 보통은 배포 전 전체 테스트를 수행할 때 테스트간 데이터 침범을 막기 위해 사용
     // 여러 테스트가 동시에 수행되면 테스트용 데이터베이스(H2)에 데이터가 그대로 남아 있어 다음 테스트 실행 시 테스트가 실패할 수 있다.
-    @After
+    @AfterEach
     public void cleanup() {
         postsRepository.deleteAll();
     }
--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -1,14 +1,14 @@
 package com.jojoldu.book.springboot.web;
 
 import com.jojoldu.book.springboot.config.auth.SecurityConfig;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
@@ -17,9 +17,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 // 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킨다.
-// 여기서는 SpringRunner라는 스프링 실행자를 사용
+// 여기서는 SpringExtension라는 스프링 실행자를 사용
 // 스프링 부트 테스트와 JUnit 사이의 연결자 역할
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 // 여러 스프링 테스트 어노테이션 중 Web(Spring MVC)에 집중할 수 있는 어노테이션
 // 선언할 경우 @Controller, @ControllerAdvice 등을 사용할 수 있다.
 // @Service, @Component, @Repository 등은 사용할 수 없다.
